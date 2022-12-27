9dfad1590036490d7fb783ea8b6ed189135b7fa4
Will Gleason <wgleason9@gmail.com>
2022-12-27 15:56:54 -0800

    remove partytown/google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,10 @@
 import { defineConfig } from "astro/config";
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
   ],
 });
