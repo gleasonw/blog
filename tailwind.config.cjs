5cd79fa203c6b8acc6f50e641ccf54f4d5b6a9f1
Will Gleason <wgleason9@gmail.com>
2023-09-10 11:32:22 +0200

    new lyno post

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		require('@tailwindcss/typography'),
-	],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontFamily: {
+      serif: '"Palatino Linotype", Palatino, "Gentium Plus", serif',
+      display: '"Palatino Linotype", Palatino, "Gentium Plus", serif',
+    },
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            fontFamily: '"Palatino Linotype", Palatino, "Gentium Plus", serif',
+          },
+        },
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
