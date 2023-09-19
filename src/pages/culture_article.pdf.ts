fc8e33a857f9a6932f0e55538cbe99e3dbc8551f
Will Gleason <wgleason9@gmail.com>
2023-09-19 20:35:40 +0200

    serve article

---
+++ src/pages/culture_article.pdf.ts
@@ -0,0 +1,15 @@
+import fs from "node:fs/promises";
+
+export async function GET({ params, request }: any) {
+  try {
+    const pdfData = await fs.readFile("../../public/culture_article.pdf");
+    const headers = {
+      "Content-Type": "application/pdf",
+      "Content-Length": pdfData.length.toString(),
+    };
+    return new Response(pdfData, { headers });
+  } catch (error) {
+    console.error(error);
+    return new Response("Internal Server Error", { status: 500 });
+  }
+}
